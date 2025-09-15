2bee46ad6493f7422d63ac257f601a4f6a23ebd0
Gustavo Moraes <gustavo.moliveira01@gmail.com>
2025-09-15 03:44:48 -0300

    chore(infrastructure-persistence-sql): keep TransactionJpaRepository

--- infrastructure-persistence-sql/src/main/java/br/com/osa/infrastructure/persistence/sql/repository/TransactionJpaRepository.java
+++ infrastructure-persistence-sql/src/main/java/br/com/osa/infrastructure/persistence/sql/repository/TransactionJpaRepository.java
@@ -1,11 +1,8 @@
 package br.com.osa.infrastructure.persistence.sql.repository;
 
 import br.com.osa.infrastructure.persistence.sql.entity.TransactionEntity;
-import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionJpaRepository extends JpaRepository<TransactionEntity, UUID> {
-
-  List<TransactionEntity> findByAccountIdOrderByCreatedAtDesc(UUID accountId);
 }
